70fbf4947f97669b400f3cca54a1238900fff702
Philip Herchenroeder <ph@dieherchenroeders.de>
2020-12-02 12:25:02 +0100

    Test Git Integration PyCharm

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 import pandas as pd
 import numpy as np
 
-vr_immo_dataset = pd.read_excel('Files/Datensatz_Projektseminar.xlsx')
-
-
-# PyCharm Dummy Code
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("Tests")
