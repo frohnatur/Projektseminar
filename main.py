9dfb1155df4b510f075db8e5fff6a2d98e2ef99c
Philip Herchenroeder <ph@dieherchenroeders.de>
2020-12-02 17:09:13 +0100

    Vorführeffekt

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ if __name__ == "__main__":
 
     with pd.option_context('display.max_rows', 5, 'display.max_columns', 17):
         print(ImmobilienAll2)
-    print("test")
+    print("Vorführeffekt")
