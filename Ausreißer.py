7cde7a6af287596218f06b27f7cfaf6812f69f1c
lennart194 <74656453+lennart194@users.noreply.github.com>
2021-04-10 14:50:59 +0200

    Delete Ausreißer.py

--- Ausreißer.py
+++
@@ -1,1 +0,0 @@
-def outlier_treatment(datacolumn):    sorted(datacolumn)    Q1,Q3 = np.percentile(datacolumn , [25,75])    IQR = Q3 - Q1    lower_range = Q1 - (1.5 * IQR)    upper_range = Q3 + (1.5 * IQR)    return lower_range,upper_rangel,u = outlier_treatment(df.angebotspreis)indexNames = df[df['angebotspreis'] > u].indexdf.drop(indexNames, inplace=True)
